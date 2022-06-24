a710e3f1a2336bd0d07b19f64af857790587e299
Benrhu <rubendiaz300000@gmail.com>
2022-06-24 20:01:56 +0200

    Modified modified ithoughtList to object

--- src/interfaces/iThought.interface.ts
+++ src/interfaces/iThought.interface.ts
@@ -1,6 +1,3 @@
-// @ts-ignore
-import { obsessions } from "./iObsession.interface.js"
-
 
 // Cada evento llama a unos iThoughts concretos.
 export enum events {
@@ -10,15 +7,12 @@ export enum events {
 }
 
 // Intrusive Thoughts => iThought
-export const iThought = {
-    fromObs: obsessions,
-    iThoughtsList: [
-        'Tengo que mirar criptos para tener más ingresos',
-        '¡Bien! Voy mejorando, en breves entro en alguna empresa chula',
-        'Venga, que vamos por buen camino.',
-        'Mierda que no llego, joder',
-        '¡¡¡ Solamente faltan 5min !!!'
-    ]
+export const iThoughtsList = {
+    criptos: 'Tengo que mirar criptos para tener más ingresos',
+    dinero: 'Esta oportunidad me parece muy importante',
+    progreso: 'Venga, que vamos por buen camino.',
+    tiempo: 'Mierda que no llego, joder',
+    invierno: 'Ya empieza a venir el frío',
 }
 
-export default { events, iThought }
+export default { events, iThoughtsList }
