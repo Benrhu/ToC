655e5ac4771187ff977908c91996966eee9a0a56
Benrhu <rubendiaz300000@gmail.com>
2022-06-24 20:02:26 +0200

    Modified intrusive function

--- src/processes/iThought.process.ts
+++ src/processes/iThought.process.ts
@@ -1,18 +1,26 @@
 // @ts-ignore
-import iThoughtInterface, { iThoughts, iThoughtsList, events } from "../interfaces/iThought.interface";
+import { iThoughtsList } from "../interfaces/iThought.interface.ts";
 
 // Función que imprime los iThoughts
-export function intrusive() {
-        let ran = Math.floor(Math.random()*iThoughtsList.length)
-        return iThoughts.iThoughtsList[ran];
+export const intrusive = () => {
+
+    let rndm = Math.floor(Math.random() * 5);
+    
+    let rndmThought = Object.values(iThoughtsList);
+
+    setInterval(() => {
+        console.log(
+            `intrusive thought: ${rndmThought[Math.floor(Math.random() * rndmThought.length)]}`
+        );
+      }, 800);
 }
 
-export function eventsUp(events: events) {
+/* export function eventsUp(events: events) {
         var rndm = Math.floor(Math.random()*5);
             if(rndm == 3){
                 let ran = Math.floor(Math.random()*events.length)
             }
-}
+} */
 
 
-export default intrusive
+export default { intrusive }
